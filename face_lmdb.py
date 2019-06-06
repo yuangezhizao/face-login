b2b444cf71697b4f259d5e12211388581bcfeff0
yuangezhizao <root@yuangezhizao.cn>
2019-06-06 14:35:33 +0800

    face_lmdb.py

--- face_lmdb.py
+++ face_lmdb.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 """
     :Author: yuangezhizao
-    :Time: 2019/5/17 0017 19:16
+    :Time: 2019/6/4 0004 23:21
     :Site: https://www.yuangezhizao.cn
     :Copyright: © 2019 yuangezhizao <root@yuangezhizao.cn>
 """
+# 通过 facenet 得到的 512 特征写入 lmdb 文件中
 import os
 
 import lmdb
@@ -15,7 +16,6 @@ import face_comm
 
 class face_lmdb:
     def add_embed_to_lmdb(self, id, vector):
-        # 通过 facenet 得到的 512 特征写入 lmdb 文件中
         self.db_file = os.path.abspath(face_comm.get_conf('lmdb', 'lmdb_path'))
         id = str(id)
         evn = lmdb.open(self.db_file)
@@ -28,10 +28,10 @@ class face_lmdb:
 if __name__ == '__main__':
     # 插入数据
     embed = face_lmdb()
-    embed.add_embed_to_lmdb(12, [1, 2, 0.888333, 0.12343430])
-
+    # embed.add_embed_to_lmdb(12, [1, 2, 0.888333, 0.12343430])
     # 遍历
-    evn = lmdb.open(embed.db_file)
+    db_file = os.path.abspath(face_comm.get_conf('lmdb', 'lmdb_path'))
+    evn = lmdb.open(db_file)
     wfp = evn.begin()
     for key, value in wfp.cursor():
-        print(key, face_comm.str_to_embed(value))
+        print(key, face_comm.str_to_embed(value.decode()))
